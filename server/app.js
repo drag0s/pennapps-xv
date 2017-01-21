7fc92a89ad9657ca8551233c7e548c286698e25b
Drag0s <me@dragosfotescu.com>
2017-01-21 08:56:50 -0500

    ADD speaking function

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const expressWs = require('express-ws')(app);
+const say = require('say');
 
 app.use(cors());
+
+app.use("/audio", express.static(__dirname + '/audio'));
 
 app.listen(3000, () => {
   console.log('Listening on 3000');
@@ -16,23 +19,46 @@ app.get('/', (req, res) => {
 });
 
 app.get('/send', (req, res) => {
-    connections.forEach(c => {
-      try {
-        c.send(req.query.msg);
-        console.log("msg sended to front");
-      } catch(e) { console.log("failed sending msg"); }
-    });
-  res.status(200).send('sent ' + req.query.msg + ' to frontend');
+  connections.forEach(c => {
+  try {
+    if (req.query.msg == '2space') {
+  c.send(' ');
+} else {
+  c.send(req.query.msg);
+}
+console.log("msg sended to front");
+} catch(e) { console.log("failed sending msg"); }
+});
+res.status(200).send('sent ' + req.query.msg + ' to frontend');
 });
 
+function toHex(str) {
+  return new Buffer(str).toString('base64');
+}
+
+app.get('/speak', (req, res) => {
+  const str = req.query.msg;
+  const file = __dirname + '/audio/' + toHex(str) + '.wav';
+  console.log(file)
+  say.export(str, 'Alex', 0.75, file, function(err) {
+    if (err) {
+      console.log('lol error');
+      return res.status(400).send(err);
+    }
+    else {
+      console.log('Text has been saved to ' + file);
+      return res.status(200).send({});
+    }
+  });
+});
 
 
 app.ws('/', (ws, req) => {
   connections.push(ws);
-  console.log("new front connected with id " + id);
-  ws.on('message', (msg) => {
-    console.log(msg);
-  });
+console.log("new front connected with id " + id);
+ws.on('message', (msg) => {
+  console.log(msg);
+});
 });
 
 app.listen(3000);
